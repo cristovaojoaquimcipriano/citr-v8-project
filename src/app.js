88ae1f590eb41a6b3a5551b7b06f2ec14a144826
Cristovão Joquim Cipriano <cristovaojoaquimcipriano@gmail.com>
2023-04-19 00:41:57 +0100

    chore: Initial Commit, React Vanila Configuration

---
+++ src/app.js
@@ -0,0 +1,9 @@
+const App = () => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Adopt Me!"),
+  ]);
+};
+
+const container = document.getElementById("root");
+const root = ReactDOM.createRoot(container);
+root.render(React.createElement(App));
