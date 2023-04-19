5bb8a2d40aab0ce4ed464f760be82b2887f06c75
Cristovão Joquim Cipriano <cristovaojoaquimcipriano@gmail.com>
2023-04-19 13:16:49 +0100

    feat: Add React-Query configuration

--- src/app.jsx
+++ src/app.jsx
@@ -2,17 +2,31 @@ import { createRoot } from "react-dom/client";
 import SearchParams from "./components/SearchParams";
 import {BrowserRouter, Routes, Route} from "react-router-dom"
 import Details from "./Details";
+import { Link } from "react-router-dom";
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query"
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries:{
+      staleTime: Infinity,
+      cacheTime: Infinity,
+      
+    }
+  }
+})
 export const App = () => {
   return (
     <BrowserRouter>
-      <div>
-        <h1>Adopt Me!</h1>
+      <QueryClientProvider client={queryClient}>
+        <header>
+          <Link to="/"> Adopt Me! </Link>
+        </header>
+
         <Routes>
           <Route path="/details/:id" element={<Details />} />
           <Route path="/" element={<SearchParams />} />
         </Routes>
-      </div>
+      </QueryClientProvider>
     </BrowserRouter>
   );
 };
