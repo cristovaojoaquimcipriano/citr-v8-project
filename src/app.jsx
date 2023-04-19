d3f900f0b815581b5f5f7bfc3be7af0f83c1bf89
Cristovão Joquim Cipriano <cristovaojoaquimcipriano@gmail.com>
2023-04-19 12:41:49 +0100

    feat: Implementing of react-router-dom

--- src/app.jsx
+++ src/app.jsx
@@ -1,12 +1,19 @@
 import { createRoot } from "react-dom/client";
 import SearchParams from "./components/SearchParams";
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+import Details from "./Details";
 
 export const App = () => {
   return (
-    <div>
-      <h1>Adopt Me!</h1>
-      <SearchParams />
-    </div>
+    <BrowserRouter>
+      <div>
+        <h1>Adopt Me!</h1>
+        <Routes>
+          <Route path="/details/:id" element={<Details />} />
+          <Route path="/" element={<SearchParams />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 };
 
