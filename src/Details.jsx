b7817c5d73c4e3be820ebf2340431d848ce36901
Cristovão Joquim Cipriano <cristovaojoaquimcipriano@gmail.com>
2023-04-19 18:18:09 +0100

    feat: Add Carousel Component

--- src/Details.jsx
+++ src/Details.jsx
@@ -1,6 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
 import { useParams } from "react-router-dom";
 import fetchPet from "./api/fetchPet";
+import Carouserl from "./components/Carousel";
 
 const Details = () => {
   const { id } = useParams();
@@ -18,6 +19,7 @@ const Details = () => {
 
   return (
     <div className="details">
+      <Carouserl images={pet.images} />
       <div>
         <h1>{pet.name}</h1>
         <h2>
