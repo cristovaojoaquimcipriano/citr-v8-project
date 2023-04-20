e5e8a4b57c0721d0f521b3edc1e742d463b63302
Cristovão Joquim Cipriano <cristovaojoaquimcipriano@gmail.com>
2023-04-20 23:37:39 +0100

    feat: Added server side rendering

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Suspense, lazy, useState } from "react";
+import { Link, Route, Routes } from "react-router-dom";
+import AdaptedPetContext from "./context/AdoptedPetContext";
+
+const Details = lazy(() => import("./Details"));
+const SearchParams = lazy(() => import("./components/SearchParams"));
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity,
+      cacheTime: Infinity,
+    },
+  },
+});
+export const App = () => {
+  const adaptedPet = useState(null);
+  return (
+    <QueryClientProvider client={queryClient}>
+      <AdaptedPetContext.Provider value={adaptedPet}>
+        <Suspense
+          fallback={
+            <div className="loading-pane">
+              <h2 className="loader">&</h2>
+            </div>
+          }
+        >
+          <header>
+            <Link to="/"> Adopt Me! </Link>
+          </header>
+
+          <Routes>
+            <Route path="/details/:id" element={<Details />} />
+            <Route path="/" element={<SearchParams />} />
+          </Routes>
+        </Suspense>
+      </AdaptedPetContext.Provider>
+    </QueryClientProvider>
+  );
+};
+
+export default App;
